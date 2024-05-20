03380fba93960f6dbd19d4e66684980a2c8fef01
Elias <Elias>
2024-05-20 10:39:29 -0400

    "Paso1_arbol_binario"

--- App_lista_tareas/js/main.js
+++ App_lista_tareas/js/main.js
@@ -1,11 +1,11 @@
-(function(){
+(function () {
 	// Variables
 	var lista = document.getElementById("lista"),
 		tareaInput = document.getElementById("tareaInput"),
 		btnNuevaTarea = document.getElementById("btn-agregar");
 
-	// Funciones
-	var agregarTarea = function(){
+	// Funciones SIN IMPLEMENTAR ARBOL DE BUSQUEDA
+	/*var agregarTarea = function () {
 		var tarea = tareaInput.value,
 			nuevaTarea = document.createElement("li"),
 			enlace = document.createElement("a"),
@@ -28,19 +28,19 @@
 
 		tareaInput.value = "";
 
-		for (var i = 0; i <= lista.children.length -1; i++) {
-			lista.children[i].addEventListener("click", function(){
+		for (var i = 0; i <= lista.children.length - 1; i++) {
+			lista.children[i].addEventListener("click", function () {
 				this.parentNode.removeChild(this);
 			});
 		}
-
 	};
-	var comprobarInput = function(){
+	
+	var comprobarInput = function () {
 		tareaInput.className = "";
 		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
 	};
 
-	var eleminarTarea = function(){
+	var eleminarTarea = function () {
 		this.parentNode.removeChild(this);
 	};
 
@@ -53,7 +53,80 @@
 	tareaInput.addEventListener("click", comprobarInput);
 
 	// Borrando Elementos de la lista
-	for (var i = 0; i <= lista.children.length -1; i++) {
+	for (var i = 0; i <= lista.children.length - 1; i++) {
 		lista.children[i].addEventListener("click", eleminarTarea);
 	}
-}());
+*/
+
+
+
+	//---------------------------CON ARBOL DE BUSQUEDA----------------------------------
+	insertNode(node, newNode);{
+		if (newNode.value < node.value) {
+			if (node.left === null) {
+				node.left = newNode;
+			} else {
+				this.insertNode(node.left, newNode);
+			}
+		} else {
+			if (node.right === null) {
+				node.right = newNode;
+			} else {
+				this.insertNode(node.right, newNode);
+			}
+		}
+	}
+	// Definición del nodo del árbol
+	class Node {
+		constructor(value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	// Definición del árbol binario de búsqueda
+	class BinarySearchTree {
+		constructor() {
+			this.root = null;
+		}
+	}
+	// funcion para insertar un valor en el árbol
+	agregarTarea = function () {
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
+
+
+
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.className = "error";
+			return false;
+		}
+
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
+
+		tareaInput.value = "";//para borrar lo escrito
+
+		for (var i = 0; i <= lista.children.length - 1; i++) {
+			lista.children[i].addEventListener("click", function () {
+				this.parentNode.removeChild(this);
+			});
+		}
+		//-------------------------------------
+		const newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+		} else {
+			this.insertNode(this.root, newNode);
+		}
+	}
+})();
